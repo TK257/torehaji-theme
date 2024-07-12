948a118cbaf21b6f70f8c22240c7c14076f2daa0
Takeo Machino <>
2024-07-12 19:42:19 +0900

    // 寄稿者にiframe権限を付与

--- functions.php
+++ functions.php
@@ -288,4 +288,12 @@ function allow_contributor_uploads()
   $contributor = get_role('contributor');
   $contributor->add_cap('upload_files');
 }
+// 寄稿者にiframe権限を付与
+add_filter('user_has_cap', 'allow_unfiltered_html', 10, 3);
+
+function allow_unfiltered_html($allcaps, $cap, $args)
+{
+  $allcaps['unfiltered_html'] = $allcaps['edit_posts'];
+  return ($allcaps);
+}
 //END
